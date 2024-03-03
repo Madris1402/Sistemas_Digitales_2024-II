cc9cbae121ac67e2fa4b40b9157be365e83bb856
Madris1402 <ricmad142@gmail.com>
2024-03-03 13:25:28 -0600

    FlipFlop Arreglado

--- Laboratorio/Practicas/flipflop_d.v
+++ Laboratorio/Practicas/flipflop_d.v
@@ -1,13 +1,30 @@
-module flipflop_d (clk, sQ);
-	
-	input 	clk;
-	output	[16:0] sQ;
-		reg		[16:0] Ff = 17'hF0000;
-	assign 	sQ = Ff; 
-	
-	always @(negedge clk)
-		begin
-			Ff = Ff;
-		end
-	
+module flipflop_d (clk, data, sQ);
+
+
+    input						clk;
+    input 			[16:0]	data;
+    output reg		[16:0]	sQ;
+
+
+
+    always @(negedge clk) begin
+        sQ <= data;
+    end
+
 endmodule
+
+
+// module flipflop_d (clk, sQ);
+	
+//	input 	clk;
+//	output	[16:0] sQ;
+	
+//	reg		[16:0] Ff = 17'hF0000;
+//	assign 	sQ = Ff; 
+	
+//	always @(negedge clk)
+//		begin
+//			Ff = Ff;
+//		end
+	
+//endmodule
